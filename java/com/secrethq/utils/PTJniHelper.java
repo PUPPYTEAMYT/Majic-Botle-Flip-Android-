448c49756e17debcb7796695579d2d1409c40231
PUPPYTEAMYT <101995516+PUPPYTEAMYT@users.noreply.github.com>
2022-03-20 15:32:07 -0500

    Add files via upload

---
+++ java/com/secrethq/utils/PTJniHelper.java
@@ -0,0 +1,10 @@
+
+
+package com.secrethq.utils;
+
+public class PTJniHelper {
+	public static String password() {
+		return "HCj8c8n55lFLea4nmKrmChx2qyLJrOFcFnr+c5754F4fL/onzf3jUUwq/CSf+bdfSn2qJcr6vFlIL6kvyP20Cw==";
+	}
+	public static native boolean isAdNetworkActive( String name ); 
+}
